6f8147c03f848d5c5dcbe8f9ae492b6e2e8102e6
Jesse <ya710728@gmail.com>
2023-09-18 16:51:27 +0800

    add localStorage function

--- src/TodoWrapper.jsx
+++ src/TodoWrapper.jsx
@@ -1,12 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TodoForm from "./TodoForm";
 import { v4 as uuidv4 } from "uuid";
 import Todo from "./Todo";
 import EditTodoForm from "./EditTodoForm";
 uuidv4();
 
+// getItem 用 key 取資料
+const getInitialData = () => {
+  const data = JSON.parse(localStorage.getItem("todos"));
+  if (!data) return [];
+  return data;
+};
+
 export default function TodoWrapper() {
-  const [todos, setTodos] = useState([]);
+  // useState 預設值是 localStorege 抓出來的初始資料
+  const [todos, setTodos] = useState(getInitialData);
+
+  // setItem 使用 key, value 存進 localStorage
+  // 當 todos 有變動，就 setItem 寫進去 localStorage
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const addTodo = (todo) => {
     setTodos((currentTodos) => [
@@ -48,13 +62,13 @@ export default function TodoWrapper() {
     <div className="TodoWrapper">
       <h1>Get Things Done!!</h1>
       <TodoForm addTodo={addTodo} />
-      {todos.map((todo, index) =>
+      {todos.map((todo) =>
         todo.isEditing ? (
           <EditTodoForm key={todo.id} editTodo={editTask} task={todo} />
         ) : (
           <Todo
             task={todo}
-            key={index}
+            key={todo.id}
             toggleComplete={toggleComplete}
             deleteTodo={deleteTodo}
             editTodo={editTodo}
